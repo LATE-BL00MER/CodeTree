a8404d6a857b19cc1560395d7c54ac130f2b4711
LATE-BL00MER <sjyoungzzang@gmail.com>
2026-01-22 14:08:15 +0900

    Title: 차례로 출력, Time: 90ms, Memory: 9MB, Status: Passed - Codetree

--- 260122/차례로 출력/print-in-order.java
+++ 260122/차례로 출력/print-in-order.java
@@ -1,5 +1,7 @@
-public static void main(String[] args) {
+public class Main {
+    public static void main(String[] args) {
         for (int i = 5; i <= 17; i++) {
             System.out.print(i + " ");
         }
     }
+}
