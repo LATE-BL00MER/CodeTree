64f77d7b39b3cdc5304ae49124c8912199d45e13
LATE-BL00MER <sjyoungzzang@gmail.com>
2026-01-28 17:27:17 +0900

    Title: 369 게임, Time: 133ms, Memory: 10MB, Status: Passed - Codetree

--- 260128/369 게임/369-game.java
+++ 260128/369 게임/369-game.java
@@ -7,8 +7,10 @@ public class Main {
         int n = sc.nextInt();
 
         for (int i = 1; i <= n; i++) {
-            if (i % 3 == 0 || (i-3) % 10 == 0 || (i-6) % 10 == 0 || (i-9) % 10 == 0)  {
-                System.out.print(0 + " ");
+            // 3의 배수이거나 숫자에 3, 6, 9가 하나라도 들어가있다면 0 출력
+            if (i % 3 == 0 || (i-3) % 10 == 0 || (i-6) % 10 == 0 || (i-9) % 10 == 0
+            || (i / 10) % 10 == 3 || (i / 10) % 10 == 6 || (i / 10) % 10 == 9)  {
+                System.out.print(0+ " ");
             } else {
                 System.out.print(i + " ");
             }
