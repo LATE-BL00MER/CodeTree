66c5c14367a3b45edef50363092e470316f87a73
LATE-BL00MER <sjyoungzzang@gmail.com>
2026-02-02 18:52:30 +0900

    Title: 친근하지 않은 수, Time: 120ms, Memory: 10MB, Status: Passed - Codetree

--- 260202/친근하지 않은 수/unfriendly-number.java
+++ 260202/친근하지 않은 수/unfriendly-number.java
@@ -7,11 +7,15 @@ public class Main {
         int n = sc.nextInt();
         int count = 0;
 
-        for (int i = 1; i < n; i++) {
-            if ((i % 2) == 0 && (i % 3) == 0 || (i % 5) == 0) {
-                count++;
+        // 친근한 수인 경우 아래의 카운트 로직을 건너뛰고 다음 숫자로 넘어간다.
+
+        for (int i = 1; i <= n; i++) {
+            if ((i % 2) == 0 || (i % 3) == 0 || (i % 5) == 0) {
+                continue;
             }
+            count++;
         }
+
         System.out.println(count);
     }
 }
