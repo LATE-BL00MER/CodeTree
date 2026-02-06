f6e4265e1bb414df683ae71ce008352c85df2a3a
LATE-BL00MER <sjyoungzzang@gmail.com>
2026-02-06 16:35:40 +0900

    Title: 3N + 1 수열, Time: 110ms, Memory: 10MB, Status: Passed - Codetree

--- 260206/3N + 1 수열/3n-plus-1-sequence.java
+++ 260206/3N + 1 수열/3n-plus-1-sequence.java
@@ -8,17 +8,17 @@ public class Main {
         int count = 0;
 
         while (true) {
+            if (n == 1) {
+                System.out.println(count);
+                break;
+            }
+            
             if (n % 2 == 0) {
                 n /= 2;
                 count++;
             } else {
                 n = (n * 3) + 1;
                 count++;
-            }
-
-            if (n == 1) {
-                System.out.println(count);
-                break;
             }
         }
     }
