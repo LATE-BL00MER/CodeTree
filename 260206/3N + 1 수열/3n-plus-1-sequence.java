0dec417ae084dc4fa95f0e12aec62c8043031bc6
LATE-BL00MER <sjyoungzzang@gmail.com>
2026-02-06 16:33:51 +0900

    Title: 3N + 1 수열, Time: 122ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260206/3N + 1 수열/3n-plus-1-sequence.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int count = 0;
+
+        while (true) {
+            if (n % 2 == 0) {
+                n /= 2;
+                count++;
+            } else {
+                n = (n * 3) + 1;
+                count++;
+            }
+
+            if (n == 1) {
+                System.out.println(count);
+                break;
+            }
+        }
+    }
+}
