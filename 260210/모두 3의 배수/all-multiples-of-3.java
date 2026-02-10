9c9c9b5406a72eebb0aab076a5be00643673b942
LATE-BL00MER <sjyoungzzang@gmail.com>
2026-02-10 13:36:17 +0900

    Title: 모두 3의 배수, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260210/모두 3의 배수/all-multiples-of-3.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        boolean satisfied = true;
+
+        for (int i = 0; i < 5; i++) {
+            int n = sc.nextInt();
+
+            if (n % 3 != 0) {
+                satisfied = false;
+            }
+        }
+
+        if (satisfied) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+
+
