d813bddbbb1b319a962953bd9fb146b37509bae5
LATE-BL00MER <sjyoungzzang@gmail.com>
2026-02-23 22:31:24 +0900

    Title: 특정 조건에 맞게 출력하기, Time: 142ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 260223/특정 조건에 맞게 출력하기/print-in-specific-conditions.java
+++ 260223/특정 조건에 맞게 출력하기/print-in-specific-conditions.java
@@ -9,15 +9,14 @@ public class Main {
         for (int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();
 
-            if (arr[i] % 2 != 0) {
-                System.out.print(arr[i] += 3);
-            } else {
-                System.out.print(arr[i] /= 2);
-            }
-            System.out.print(" ");
-
             if (arr[i] == 0) {
                 break;
+            }
+
+            if (arr[i] % 2 != 0) {
+                System.out.print((arr[i] += 3) + " ");
+            } else {
+                System.out.print((arr[i] /= 2) + " ");
             }
         }
     }
